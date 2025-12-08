d023f195e88777834d7c9076f3615b3beb35a887
yeeun0702 <kimyeeun0702@naver.com>
2025-12-08 18:25:30 +0900

    [TS](section09): 조건부 타입 소개

---
+++ one-bite-typescript/section09/src/chapter00.ts
@@ -0,0 +1,42 @@
+/**
+ * 조건부 타입
+ * */
+
+type A = number extends  string ? string : number;
+
+type ObjA = {
+    a: number;
+};
+
+type ObjB = {
+    a: number;
+    b: number;
+};
+
+type B = ObjB extends ObjA ? number : string;
+
+/**
+ * 제네릭과 조건부 타입
+ * */
+type StringNumberSwitch<T> = T extends number ?string : number;
+let varA: StringNumberSwitch<number>;
+// string
+
+let varB: StringNumberSwitch<string>;
+// number
+
+function removeSpaces<T>(text: T): T extends string ? string : undefined;
+function removeSpaces(text: any) {
+    if (typeof text === "string") {
+        return text.replaceAll(" ", "");
+    } else {
+        return undefined;
+    }
+}
+
+let result = removeSpaces("hi im winterlood");
+// string
+
+let result2 = removeSpaces(undefined);
+// undefined
+
