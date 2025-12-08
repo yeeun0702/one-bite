9ae785ad95d3fbba6fe2db3692801d14d5d15ef5
yeeun0702 <kimyeeun0702@naver.com>
2025-12-08 16:24:24 +0900

    [TS](section07): map, forEach 메서드 타입 정의하기

---
+++ one-bite-typescript/section07/src/chapter02.ts
@@ -0,0 +1,43 @@
+/*
+* map 메서드
+* */
+
+const arr = [1, 2, 3];
+const newArr = arr.map((it) => it *2);
+// [2, 4, 6]
+
+function map<T ,U>(arr: T[], callback:(iten : T) => U){
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        result.push(callback(arr[i]));
+    }
+
+    return result;
+}
+
+map(arr, (it) => it * 2);
+map(["hi", "hello"], (it) => parseInt(it));
+
+
+
+/*
+* forEach
+* */
+const arr2 = [1, 2, 3];
+
+arr2.forEach((it) => console.log(it));
+// 출력 : 1, 2, 3
+
+function forEach<T>(arr: T[], callback: (item: T) => void) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i]);
+    }
+}
+
+forEach(arr2, (it) => {
+    console.log(it.toFixed());
+});
+
+forEach(['123', '456'], (it) => {
+    it;
+})
