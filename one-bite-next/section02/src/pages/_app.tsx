5ff63f9df623ff04cc46bab334ed73dcbb6c207c
yeeun0702 <kimyeeun0702@naver.com>
2025-12-17 18:01:03 +0900

    [Next.js](section02): 페이지별 레이아웃 설정하기

--- one-bite-next/section02/src/pages/_app.tsx
+++ one-bite-next/section02/src/pages/_app.tsx
@@ -1,15 +1,25 @@
-import "@/styles/globals.css";
-import type {AppProps} from "next/app";
 import GlobalLayout from "@/components/global-layout";
+import "@/styles/globals.css";
+import { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { ReactNode } from "react";
 
+type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactNode) => ReactNode;
+};
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App({
+                                Component,
+                                pageProps,
+                            }: AppProps & {
+    Component: NextPageWithLayout;
+}) {
+    const getLayout =
+        Component.getLayout ?? ((page: ReactNode) => page);
 
     return (
         <GlobalLayout>
-            <Component {...pageProps} />
+            {getLayout(<Component {...pageProps} />)}
         </GlobalLayout>
     );
-
-
 }
--- one-bite-next/section02/src/pages/search/index.tsx
+++ one-bite-next/section02/src/pages/search/index.tsx
@@ -1,9 +1,15 @@
-import {useRouter} from 'next/router';
+import SearchableLayout from "@/components/searchable-layout";
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
-export default function Page(){
+export default function Page() {
     const router = useRouter();
 
-    const {q} = router.query;
+    const { q } = router.query;
 
-    return <h1>Search {q}</h1>
+    return <h1>Search {q}</h1>;
 }
+
+Page.getLayout = (page: ReactNode) => {
+    return <SearchableLayout>{page}</SearchableLayout>;
+};
