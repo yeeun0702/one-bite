f16827779663bc500bfaee3ec1c57a2ff8870b62
yeeun0702 <kimyeeun0702@naver.com>
2025-12-19 12:31:44 +0900

    [Next.js](section02): SSR 2. 실습

--- one-bite-next/section02/src/pages/book/[id].tsx
+++ one-bite-next/section02/src/pages/book/[id].tsx
@@ -1,4 +1,6 @@
 import style from "./[id].module.css";
+import {GetServerSidePropsContext} from "next";
+import fetchOneBook from "@/lib/fetch-one-book";
 
 const mockData = {
     id: 1,
@@ -12,7 +14,24 @@ const mockData = {
         "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg",
 };
 
-export default function Page() {
+export const getServerSideProps = async (
+    context: GetServerSidePropsContext
+) => {
+    const id = context.params!.id;
+    const book = await fetchOneBook(Number(id));
+
+    return {
+        props: {
+            book,
+        },
+    };
+};
+
+
+export default function Page({
+                                 book,
+                             }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    if (!book) return "문제가 발생했습니다 다시 시도하세요";
     const {
         id,
         title,
@@ -21,7 +40,7 @@ export default function Page() {
         author,
         publisher,
         coverImgUrl,
-    } = mockData;
+    } =  book;
 
     return (
         <div className={style.container}>
--- one-bite-next/section02/src/pages/index.tsx
+++ one-bite-next/section02/src/pages/index.tsx
@@ -1,43 +1,41 @@
 // CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import {ReactNode, useEffect} from "react";
-import books from "@/mock/books.json";
+import { ReactNode, useEffect } from "react";
 import BookItem from "@/components/book-item";
-import {InferGetServerSidePropsType} from "next";
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = () => {
-    // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+export const getServerSideProps = async () => {
+    const [allBooks, recoBooks] = await Promise.all([
+        fetchBooks(),
+        fetchRandomBooks(),
+    ]);
 
-
-    const data = "hello";
-    console.log("서버 사이드 프롭스")
     return {
         props: {
-            data,
+            allBooks,
+            recoBooks,
         },
     };
 };
 
-export default function Home({data} : InferGetServerSidePropsType<typeof getServerSideProps>) {
-
-    console.log(data)
-    useEffect(() => {
-        console.log(window);
-    }, [])
-
-    console.log(data);
+export default function Home({
+                                 allBooks,
+                                 recoBooks,
+                             }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
         <div className={style.container}>
             <section>
                 <h3>지금 추천하는 도서</h3>
-                {books.map((book) => (
+                {recoBooks.map((book) => (
                     <BookItem key={book.id} {...book} />
                 ))}
             </section>
             <section>
                 <h3>등록된 모든 도서</h3>
-                {books.map((book) => (
+                {allBooks.map((book) => (
                     <BookItem key={book.id} {...book} />
                 ))}
             </section>
